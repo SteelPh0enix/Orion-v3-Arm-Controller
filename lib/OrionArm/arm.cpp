e5bfa2602b1fcc4db81943fe1ccaa8ecf451d44c
SteelPh0enix <steelphoenix2137@gmail.com>
2019-09-12 13:03:49 +0200

    Basic arm code structure

---
+++ lib/OrionArm/arm.cpp
@@ -0,0 +1,9 @@
+#include "arm.hpp"
+
+OrionArm::OrionArm() {}
+
+void OrionArm::initialize() {}
+
+void OrionArm::setMotorPower(OrionArm::Motor motor, int speed) {}
+
+void OrionArm::stop() {}
--- lib/OrionArm/arm.hpp
+++ lib/OrionArm/arm.hpp
@@ -1,8 +1,30 @@
 #pragma once
 #include <Arduino.h>
+#include <cstdint>
 #include <mc33926.hpp>
 
 class OrionArm {
  public:
+  enum class Motor {
+    Turntable,
+    GrasperRotation,
+    GrasperX,
+    GrasperY,
+    LowerActuator,
+    UpperActuator
+  };
+
+  OrionArm();
+
+  void initialize();
+  void setMotorPower(Motor motor, int power);
+  void stop();
+
  private:
+  MC33926 m_turntableMotor;
+  MC33926 m_grasperRotationMotor;
+  MC33926 m_grasperXAxisMotor;
+  MC33926 m_grasperYAxisMotor;
+  std::uint8_t m_lowerActuatorPWMPin;
+  std::uint8_t m_upperActuatorPWMPin;
 };
